9c390883089a5c3273c585407441e3a55b84dd23
ironsummit72 <ironsumit72@gmail.com>
2024-04-11 12:57:32 +0530

    check if user authenticated or not

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
-import "./App.css";
+import { useEffect } from 'react';
+import {Outlet} from 'react-router-dom'
+import {useDispatch} from 'react-redux';
+import AuthService from './appwrite/AuthService'; 
+import { login,logout } from './stateManager/redux/authSlice';
+
 function App() {
+ const dispatch=useDispatch();
+  const authService=new AuthService();
+  useEffect(()=>{
+    const currentUser=authService.getCurrentUserAccount();
+    currentUser.then(res=>{
+      if(res)
+      {
+        dispatch(login(res))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+  },[])
   return <>
+  <Outlet/>
   </>;
 }
 export default App;
