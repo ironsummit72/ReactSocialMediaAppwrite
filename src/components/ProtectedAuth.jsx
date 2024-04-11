e09c588d73c4fb19f4093aaf82798ec6e6808e07
ironsummit72 <ironsumit72@gmail.com>
2024-04-11 12:58:27 +0530

    a protected route added

--- src/components/ProtectedAuth.jsx
+++ src/components/ProtectedAuth.jsx
@@ -1,22 +1,11 @@
-
-import { useEffect, useState } from 'react';
-import {useSelector} from 'react-redux';
-import { Link } from 'react-router-dom';
-
-
-
+import {useSelector} from 'react-redux'
+import { Link } from 'react-router-dom'
 function ProtectedAuth({children}) {
-
-  const auth=useSelector((state)=>{return state.Auth.userAuth})
-
-// useEffect(()=>{
-
-// },[])
-
-
+  const auth=useSelector(state=>state.isUserAuthenticated)
+  
   return auth? (
     <div>{children}</div>
-  ):(<div>Please <Link className='text-blue-600 font-bold hover:underline'  to={'/login'} replace={true}>Login</Link> </div>)
+  ):<div>Login <Link to='/login' replace={true} className='text-blue-600 font-semibold'>here</Link></div>
 }
 
 export default ProtectedAuth
