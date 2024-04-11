dce86a96739a643e356a8db4122e4f22a96581e5
ironsummit72 <ironsumit72@gmail.com>
2024-04-11 12:56:58 +0530

    added redux store and provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { Provider } from 'react-redux';
-import store from './redux/store.js';
+import store from './stateManager/redux/store.js';
+import {Provider} from 'react-redux';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
-import Test from './pages/Test.jsx';
 import ProtectedAuth from './components/ProtectedAuth.jsx';
 import Home from './pages/Home.jsx';
 import Profile from './pages/Profile.jsx';
-
 const router = createBrowserRouter([
   {
     path: '/',
@@ -35,21 +33,13 @@ const router = createBrowserRouter([
       {
         path: '/',
         element:<ProtectedAuth> <Profile /></ProtectedAuth>
-      },
-      {
-        path: '/currentuser', //TODO:delete later
-        element: (
-          <ProtectedAuth authentication>
-            <Test />
-          </ProtectedAuth>
-        )
       }
     ]
-  } //TODO: wrap in a protected Routes
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <RouterProvider router={router} />
-  </Provider>
+<Provider store={store}>
+  <RouterProvider router={router} />
+</Provider>
 );
