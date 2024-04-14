879fabbe9143876ede5256054f91de9915086a2a
ironsummit72 <ironsumit72@gmail.com>
2024-04-14 14:06:21 +0530

    Profile page added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,17 @@
+import Navbar from '@/components/Navbar';
+import  {useSelector} from 'react-redux';
+import UserService from '@/appwrite/UserService';
 export default function Profile() {
+  const user=new UserService();
+  user.checkUserDisplay().then(res=>{console.log(res)}).catch(err=>{console.log(err)})
+const state=useSelector(state=>state.userData)
+console.log(state);
+  
   return (
     <>
-     
-      <h1>Profile</h1>
+     <Navbar/>
+      <h1>Profile {state.$id}</h1>
+
     </>
   );
 }
