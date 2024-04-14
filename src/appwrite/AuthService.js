0b98b3444d8294c2b55d960c3fda4097134345e3
ironsummit72 <ironsumit72@gmail.com>
2024-04-14 13:39:32 +0530

    appwrite auth service added for authentication related work

--- src/appwrite/AuthService.js
+++ src/appwrite/AuthService.js
@@ -2,44 +2,26 @@ import AppwriteService from './config';
 
 export default class AuthService extends AppwriteService {
   async getCurrentUserAccount() {
+    return await this.account.get();
+  }
+  async createUserAccount(username, fullName, email, password) {
+    return await this.account.create(username, email, password, fullName);
+  }
+  async login(email, password) {
+    return await this.account.createEmailSession(email, password);
+  }
+  async logout() {
     try {
-      return await this.account.get();
+      return await this.account.deleteSessions();
     } catch (error) {
       console.log(error);
-      return null;
     }
   }
-  async createUserAccount(username,fullName, email, password) {
-    try {
-      return await this.account.create(username, email, password, fullName);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  async login(email,password){
-    try {
-        return await this.account.createEmailSession(email,password);
-    } catch (error) {
-        console.log(error);
-    }
-  }
-  async logout(){
-    try {
-
-      return await this.account.deleteSessions()
-
-    } catch (error) {
-
-      console.log(error);
-    }
-  }
-  async listSessions()
-  {
+  async listSessions() {
     try {
       return await this.account.listSessions();
     } catch (error) {
       console.log(error);
     }
   }
-  
 }
