4b299653a57fe88baa527b976d5133a9b6b1c139
ironsummit72 <ironsumit72@gmail.com>
2024-04-15 23:22:47 +0530

    feed added

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,21 +1,15 @@
 import Navbar from '@/components/Navbar';
 import { useSelector } from 'react-redux';
-import UserService from '@/appwrite/UserService';
 import Container from '@/components/Container';
 import CreatePostCard from '@/components/CreatePostCard';
 export default function Feed() {
-  const user = new UserService();
-
-   const userId=useSelector(state=>state.userData.$id)
-
+  const userId = useSelector((state) => state.userData.$id);
   return (
     <div className="">
       <Navbar />
-
       <Container>
-        <CreatePostCard username={userId}/>
+        <CreatePostCard className={'ml-[32%]'} username={userId} />
       </Container>
-
     </div>
   );
 }
