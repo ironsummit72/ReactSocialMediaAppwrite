3149f548b7bdb5406b071b4af032596722cdb9b9
ironsummit72 <ironsumit72@gmail.com>
2024-04-15 11:42:15 +0530

    setDisplayPicture and setCoverPicture method added

--- src/appwrite/PostService.js
+++ src/appwrite/PostService.js
@@ -1,7 +1,21 @@
-import AppwriteService from './config';
+import envconf from '@/conf/envConf';
+import AppwriteService, { ID, Query } from './config';
+
 
 export default class PostService extends AppwriteService {
   createPost() {}
-  createDisplayPicture() {}
+ async setDisplayPicture(imageFile,userId) {
+    const fileResponse=await this.storage.createFile(envconf.appWriteBucketIdUsersProfile,ID.unique(),imageFile);
+    const documentId=await this.databases.listDocuments(envconf.appWriteDatabase,envconf.appWriteCollectionIdUsers,[ Query.equal('username',userId),])
+    const dbResponse =await this.databases.updateDocument(envconf.appWriteDatabase,envconf.appWriteCollectionIdUsers,documentId.documents[0].$id,{displayPictureFileId:fileResponse.$id})
+   return dbResponse
+    
+  }
+  async setCoverPicture(imageFile,userId) {
+    const fileResponse=await this.storage.createFile(envconf.appWriteBucketIdUsersProfile,ID.unique(),imageFile);
+    const documentId=await this.databases.listDocuments(envconf.appWriteDatabase,envconf.appWriteCollectionIdUsers,[ Query.equal('username',userId),])
+    const dbResponse =await this.databases.updateDocument(envconf.appWriteDatabase,envconf.appWriteCollectionIdUsers,documentId.documents[0].$id,{coverPictureFileId:fileResponse.$id})
+   return dbResponse
+  }
   createReel() {}
 }
