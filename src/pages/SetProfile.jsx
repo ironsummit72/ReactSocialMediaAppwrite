188d296faf10b3be72990449f7992b51fe9891eb
ironsummit72 <ironsumit72@gmail.com>
2024-04-15 11:44:17 +0530

    user will be redirect to setprofile/dp

--- src/pages/SetProfile.jsx
+++ src/pages/SetProfile.jsx
@@ -1,27 +1,15 @@
-import { Button } from '@/shadcomponents/ui/button';
-import { Outlet } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import { buttonVariants } from '@/shadcomponents/ui/button';
-import { useState } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
 function SetProfile() {
-  const [page, setPage] = useState(0);
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate('/setprofile/dp', { replace: true });
+  }, []);
   return (
-    <div className="relative h-screen overflow-hidden">
-      <Outlet context={[page, setPage]} />
-      {page == 0 ? (
-        <Button asChild className="absolute bottom-5 right-10">
-          <Link className={buttonVariants({ variant: 'secondary' })} to="/setprofile/cover" replace={true}>
-            Next
-          </Link>
-        </Button>
-      ) : (
-        <Button asChild className="absolute bottom-5 right-10">
-          <Link className={buttonVariants({ variant: 'secondary' })} to="/" replace={true}>
-            Finish
-          </Link>
-        </Button>
-      )}
-    </div>
+    <>
+      <Outlet />
+    </>
   );
 }
 export default SetProfile;
