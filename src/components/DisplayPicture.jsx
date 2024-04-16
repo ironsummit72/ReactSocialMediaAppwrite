1454224d89e357fa5bedd740a8f6d9fed0c25ea3
ironsummit72 <ironsumit72@gmail.com>
2024-04-16 23:50:47 +0530

    added image url prop

--- src/components/DisplayPicture.jsx
+++ src/components/DisplayPicture.jsx
@@ -2,19 +2,27 @@ import PostService from '@/appwrite/PostService';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
-function DisplayPicture( {className} ) {
+function DisplayPicture({ className, imgUrl, alt }) {
   const [displayPicture, setDisplayPicture] = useState('');
   const postservice = new PostService();
   const userID = useSelector((state) => state.userData.$id);
   useEffect(() => {
     postservice.getProfilePicture(userID).then((res) => setDisplayPicture(res.href));
   }, []);
-  return (
+  return imgUrl ? (
     <>
       <img
-        className={className??'w-60 h-60 rounded-full object-cover'}
+        className={className ?? 'w-60 h-60 rounded-full object-cover'}
+        src={imgUrl}
+        alt={alt}
+      />
+    </>
+  ) : (
+    <>
+      <img
+        className={className ?? 'w-60 h-60 rounded-full object-cover'}
         src={displayPicture}
-        alt=""
+        alt={''}
       />
     </>
   );
