9ee2d4c1a81a5365398087f46b3c0013d7cf1223
ironsummit72 <ironsumit72@gmail.com>
2024-04-17 01:26:24 +0530

    moved to Dialogs directory

--- src/components/CreatePostDialog.jsx
+++
@@ -1,9 +0,0 @@
-
-
-function CreatePostDialog() {
-  return (
-    <div>CreatePostDialog</div>
-  )
-}
-
-export default CreatePostDialog
---
+++ src/components/Dialogs/CreatePostDialog.jsx
@@ -0,0 +1,22 @@
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/shadcomponents/ui/alert-dialog"
+import { Button } from "@/shadcomponents/ui/button"
+function CreatePostDialog() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CreatePostDialog
