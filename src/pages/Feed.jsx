1259459affb8ea46edc800f951fbd9f9a5674aa5
ironsummit72 <ironsumit72@gmail.com>
2024-04-18 02:07:29 +0530

    now users can create post just by clicking photos/videos buttton or the card

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -4,7 +4,13 @@ import CreatePostCard from '@/components/CreatePostCard';
 import CreateStoryCard from '@/components/CreateStoryCard';
 import SideNavigation from '@/components/SideNavigation';
 import StoryContainer from '@/components/containers/StoryContainer';
+import CreatePostDialog from '@/components/Dialogs/CreatePostDialog';
+import { useState } from 'react';
 export default function Feed() {
+  const [postDialogOpen,setPostDialogOpen]=useState(false);
+  const createPostHandler=()=>{
+    setPostDialogOpen(true)
+  }
   return (
     <>
       <Navbar />
@@ -14,7 +20,8 @@ export default function Feed() {
           <StoryContainer className={`h-80 mt-10 flex items-center gap-5 p-4`}>
             <CreateStoryCard />
           </StoryContainer>
-          <CreatePostCard className={`m-0 m-auto`} username="" />
+          <CreatePostCard className={`m-0 m-auto`} username=""   whatsOnMindOnCLick={createPostHandler}/>
+          <CreatePostDialog open={postDialogOpen} onCancelHandler={()=>{setPostDialogOpen(false)}}/>
         </Container>
       </div>
     </>
