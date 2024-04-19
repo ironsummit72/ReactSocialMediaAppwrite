c035742bc5d9baef33266d1b02eb736dfec7e3b1
ironsummit72 <ironsumit72@gmail.com>
2024-04-20 00:53:08 +0530

    createPost method added so that users can create posts

--- src/appwrite/PostService.js
+++ src/appwrite/PostService.js
@@ -2,7 +2,6 @@ import envconf from '@/conf/envConf';
 import AppwriteService, { ID, Query } from './config';
 
 export default class PostService extends AppwriteService {
-  createPost() {}
   async setDisplayPicture(imageFile, userId) {
     const fileResponse = await this.storage.createFile(
       envconf.appWriteBucketIdUsersProfile,
@@ -42,23 +41,54 @@ export default class PostService extends AppwriteService {
     return dbResponse;
   }
   createReel() {}
-  async getProfilePicture(username){
-    
+  async getProfilePicture(username) {
     try {
-      const fileId=await this.databases.listDocuments(envconf.appWriteDatabase,envconf.appWriteCollectionIdUsers,[Query.equal('username', username)]) 
-      return this.storage.getFileView(envconf.appWriteBucketIdUsersProfile, fileId.documents[0].displayPictureFileId);
+      const fileId = await this.databases.listDocuments(
+        envconf.appWriteDatabase,
+        envconf.appWriteCollectionIdUsers,
+        [Query.equal('username', username)]
+      );
+      return this.storage.getFileView(
+        envconf.appWriteBucketIdUsersProfile,
+        fileId.documents[0].displayPictureFileId
+      );
     } catch (error) {
       console.log(error);
       return null;
     }
   }
-  async getCoverPicture(username){
+  async getCoverPicture(username) {
     try {
-      const fileId=await this.databases.listDocuments(envconf.appWriteDatabase,envconf.appWriteCollectionIdUsers,[Query.equal('username', username)])
-      return this.storage.getFileView(envconf.appWriteBucketIdUsersProfile, fileId.documents[0].coverPictureFileId);
+      const fileId = await this.databases.listDocuments(
+        envconf.appWriteDatabase,
+        envconf.appWriteCollectionIdUsers,
+        [Query.equal('username', username)]
+      );
+      return this.storage.getFileView(
+        envconf.appWriteBucketIdUsersProfile,
+        fileId.documents[0].coverPictureFileId
+      );
     } catch (error) {
       console.log(error);
       return null;
     }
   }
+  async createPost(userId, Files,caption,visibility) {
+    const documentId = await this.databases.listDocuments(
+      envconf.appWriteDatabase,
+      envconf.appWriteCollectionIdUsers,
+      [Query.equal('username', userId)]
+    );
+    const currentUserDocumentId = documentId.documents[0].$id;
+    let bucketIds=[]
+    for(const files of Files)
+    {
+     const userFile=await this.storage.createFile(envconf.appWriteBucketIdUsersPost,ID.unique(),files);
+     bucketIds.push(userFile.$id);
+    }
+    const createPostData=await this.databases.createDocument(envconf.appWriteDatabase,envconf.appWriteCollectionIdPost,ID.unique(),{caption:caption,fileId:bucketIds,users:currentUserDocumentId,visibility:visibility})
+    console.log(createPostData);
+    return createPostData;
+  }
+
 }
