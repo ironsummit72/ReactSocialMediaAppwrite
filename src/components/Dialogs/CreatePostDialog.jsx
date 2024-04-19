e7b42453e300dd6977dffd08469750ff5845d15a
ironsummit72 <ironsumit72@gmail.com>
2024-04-19 18:49:25 +0530

    react form hook added

--- src/components/Dialogs/CreatePostDialog.jsx
+++ src/components/Dialogs/CreatePostDialog.jsx
@@ -1,14 +1,13 @@
 import {
   AlertDialog,
   AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
 } from '@/shadcomponents/ui/alert-dialog';
 import { Button } from '@/shadcomponents/ui/button';
-import { X, Earth, Users, Lock,ImageUp } from 'lucide-react';
-import DisplayPicture from '../DisplayPicture';
+import { X, Earth, Users, Lock, ImageUp } from 'lucide-react';
+import DisplayPicture from '@/components/DisplayPicture';
 import { useSelector } from 'react-redux';
-import {  useState } from 'react';
+import { useForm,Controller } from 'react-hook-form';
+import { useState } from 'react';
 import {
   Select,
   SelectContent,
@@ -21,11 +20,9 @@ import {
 import { Textarea } from '@/shadcomponents/ui/textarea';
 import { Input } from '@/shadcomponents/ui/input';
 import { Label } from '@/shadcomponents/ui/label';
-function CreatePostDialog({
-  open,
-  onOpenChange,
-  onCancelHandler
-}) {
+import { Form } from '@/shadcomponents/ui/form';
+
+function CreatePostDialog({ open, onOpenChange, onCancelHandler }) {
   const [selectedFiles, setSelectedFiles] = useState([]);
   const [dragActive, setDragActive] = useState(false);
   const userData = useSelector((state) => state.userData);
@@ -36,41 +33,46 @@ function CreatePostDialog({
   const handleDrop = (event) => {
     event.preventDefault();
     setDragActive(false);
-    if(event.dataTransfer.files.length>10)
-    {
-      alert('you can only upload 10 images and videos in one post')
-    }else{
-
-      setSelectedFiles([...event.dataTransfer.files])
+    if (event.dataTransfer.files.length > 10) {
+      alert('you can only upload 10 images and videos in one post');
+    } else {
+      setSelectedFiles([...event.dataTransfer.files]);
     }
-  }
-  const handleFileUpload=(event) => {
-    if(event.target.files.length>10)
-    {
-      alert('you can only upload 10 images and videos in one post')
-    }else{
-      setSelectedFiles([...event.target.files])
+  };
+  const handleFileUpload = (event) => {
+    if (event.target.files.length > 10) {
+      alert('you can only upload 10 images and videos in one post');
+    } else {
+      setSelectedFiles([...event.target.files]);
     }
-  }
+  };
+  console.log(selectedFiles);
+  const form=useForm()
+  
+  const submitHandler = (data) => {
+    console.log(data);
+  } 
   return (
     <AlertDialog open={open} onOpenChange={onOpenChange}>
-
-      <AlertDialogContent className="h-[90%]">
-      <div className={`h-10 flex justify-center items-center border-b-2`}>
-            <h1 className="font-bold text-lg">Create Post</h1>
-            <Button
-              onClick={onCancelHandler}
-              variant="ghost"
-              className="w-14 h-14 rounded-full relative left-32">
-              <X />
-            </Button>
-          </div>
+      <AlertDialogContent className="h-[90%] flex-col items-start">
+        <div className={`h-10  flex justify-center items-center border-b-2`}>
+          <h1 className="font-bold text-lg">Create Post</h1>
+          <Button
+            onClick={onCancelHandler}
+            variant="ghost"
+            className="w-14 h-14 rounded-full relative left-32">
+            <X />
+          </Button>
+        </div>
+        <Form>
+        <form noValidate onSubmit={form.handleSubmit(submitHandler)} className="">
           <div className="h-20 profileInfo bg-white p-2 ">
             <DisplayPicture className={`w-14 h-14 rounded-full`} />
             <h1 className="font-semibold relative bottom-14 left-16">{userData.name}</h1>
-            <Select>
-              <SelectTrigger className="w-[140px] relative bottom-12 left-16 bg-gray-200">
-                <SelectValue placeholder="Post Visibility" defaultValue={`PUBLIC`} />
+            <Controller control={form.control} name='postvisibility' defaultValue={`PUBLIC`} render={({field:{onChange,value,name}})=>(
+            <Select  onValueChange={onChange} value={value}  name={name}>
+              <SelectTrigger  className="w-[140px] relative bottom-12 left-16 bg-gray-200">
+                <SelectValue placeholder="Post Visibility"  />
               </SelectTrigger>
               <SelectContent>
                 <SelectGroup>
@@ -93,46 +95,53 @@ function CreatePostDialog({
                 </SelectGroup>
               </SelectContent>
             </Select>
+             )}/>
           </div>
-
           <div className="cap overflow-auto ">
-            <Textarea className="border-none outline-0 text-md focus:outline-0 resize-none" placeholder={`What's on your mind, ${userData.name.split(' ')[0]}?`}/>
+            <Textarea {...form.register('caption')}
+              className="border-none outline-0 text-md focus:outline-0 resize-none"
+              placeholder={`What's on your mind, ${userData.name.split(' ')[0]}?`}
+            />
           </div>
-        <AlertDialogHeader className="flex-col   h-full  overflow-x-hidden no-scrollbar">
-          <div className="addcontent w-full h-full border  rounded-lg min-h-[70rem]  flex justify-center items-start" onDragOver={handleDrag} onDragEnter={handleDrag} onDragLeave={handleDrag} onDrop={handleDrop} >
-           {selectedFiles.length===0? <div  className={`w-[98%] h-[38%] border-gray-600 bg-gray-100 hover:bg-gray-200 ${dragActive?"bg-gray-500":''} cursor-auto 400 rounded-lg m-1 flex justify-center items-center`}>
-            <Label
-            htmlFor="upload-dp"
-            className="border-gray-500 h-full  w-full flex items-center border text-black font-medium text-lg p-3 rounded-sm flex-col justify-center">
-              <ImageUp/>
-            Add photos/videos
-            <span className="block text-sm text-gray-500">or drag and drop</span>
-            <Input
-              accept="image/*,video/*"
-              onChange={handleFileUpload}
-              type="file"
-              className="sr-only"
-              id="upload-dp"
-              multiple
-              />
-          </Label>
-            </div>:
-            <div className={`w-[98%] h-full rounded-lg mediacontent grid grid-cols-2 grid-rows-auto gap-x-2 gap-y-2`}>
-             {selectedFiles.map((file)=>{
+          {selectedFiles.length === 0 ? (
+            <div
+              onDragOver={handleDrag}
+              onDragEnter={handleDrag}
+              onDragLeave={handleDrag}
+              onDrop={handleDrop}
+              className={`filesDrop w-full h-96 bg-gray-200 rounded-md border-gray-500 border-2 flex justify-center items-center ${dragActive?"bg-gray-500":''}`}>
+              <Label
+                htmlFor="upload-dp"
+                className="border-gray-500 h-full  w-full flex items-center border text-black font-medium text-lg p-3 rounded-sm flex-col justify-center">
+                <ImageUp />
+                Add photos/videos
+                <span className="block text-sm text-gray-500">or drag and drop</span>
+                <Input
+                  accept="image/*,video/*"
+                  onChange={handleFileUpload}
+                  type="file"
+                  className="sr-only"
+                  id="upload-dp"
+                  multiple
+                />
+              </Label>
+            </div>
+          ) : (
+            <div className='w-full h-96 overflow-auto no-scrollbar'>
+               <div className="preview grid grid-cols-2 grid-rows-2 gap-y-4 gap-x-4">
+               {selectedFiles.map((file)=>{
                if(file.type.split('/')[0]==='video'){
                  return <video className='w-60 rounded-md' key={file.name} autoPlay={true} muted={true} src={URL.createObjectURL(file)}/>
                 }else{
                   return <img className='w-60 rounded-md' key={file.name} src={URL.createObjectURL(file)} alt="" />
                 }
               })}
-              </div>}
-          </div>
-        </AlertDialogHeader>
-        <AlertDialogFooter className={`bg-red-500`}>
-          <div className="buttonContainer w-full bg-red-500  relative  ">
-          <Button type='submit' className="w-full p-5">Post</Button>
-          </div>
-        </AlertDialogFooter>
+               </div>
+            </div>
+          )}
+          <Button className='mt-12 w-full' type='submit'>Post</Button>
+        </form>
+        </Form>
       </AlertDialogContent>
     </AlertDialog>
   );
