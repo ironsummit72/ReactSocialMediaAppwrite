93bc350cfcfedf59935082cc66cf2d7e99eede2b
ironsummit72 <ironsumit72@gmail.com>
2024-04-22 19:56:37 +0530

    custom video player component added

---
+++ src/components/VideoPlayer.jsx
@@ -0,0 +1,12 @@
+import { useState, useRef } from 'react';
+
+function VideoPlayer({ src, className ,autoPlay}) {
+  const [isPlaying, setIsPlaying] = useState(true);
+  return (
+
+      <video src={src} className={className} autoPlay={autoPlay} />
+
+  );
+}
+
+export default VideoPlayer;
