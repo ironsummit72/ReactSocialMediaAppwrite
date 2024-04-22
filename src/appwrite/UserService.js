65a74e2398c1b38ac332fcfbba60e43a7e1226d0
ironsummit72 <ironsumit72@gmail.com>
2024-04-22 19:54:01 +0530

    name has been added to database

--- src/appwrite/UserService.js
+++ src/appwrite/UserService.js
@@ -3,13 +3,13 @@ import AppwriteService, { ID, Query } from './config';
 
 
 export default class UserService extends AppwriteService {
-  async createUser(username) {
+  async createUser(username,name) {
     try {
       return await this.databases.createDocument(
         envconf.appWriteDatabase,
         envconf.appWriteCollectionIdUsers,
         ID.unique(),
-        { username: username }
+        { username: username,name: name}
       );
     } catch (err) {
       console.log(err);
